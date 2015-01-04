7e6e1c85de78218882846217ba1f79eb168b5e3f
Tom Bates <tom.bates@onthebeach.co.uk>
2015-01-04 21:04:24 +0000

    Initial Commit

---
+++ SHTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHTimer
+//
+//  Created by Tom Bates on 04/01/2015.
+//  Copyright (c) 2015 SportHub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
