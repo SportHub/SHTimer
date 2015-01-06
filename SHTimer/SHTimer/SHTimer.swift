a6d945db8e22f38662ff7e26914f24befa6eefe3
Tom Bates <tom.bates@onthebeach.co.uk>
2015-01-06 23:20:01 +0000

    update for function calls

--- SHTimer/SHTimer/SHTimer.swift
+++ SHTimer/SHTimer/SHTimer.swift
@@ -1,20 +1,21 @@
 import Foundation
 
 class SHTimer: NSObject {
-
+  
   var timer :NSTimer?
   let time :Float?
   var repeats = false
   var fireTarget :AnyObject?
   var fireSelector :Selector?
   var fireBlock :(() -> Void)?
-
+  var fireFunction :(() -> Void)?
+  
   class func startTimeWithDurtion (duration :Float, repeats :Bool, target :AnyObject, selector :Selector) -> SHTimer {
     let timer = SHTimer()
     timer.startTimeWithDurtion(duration, repeats: repeats, target: target, selector: selector)
     return timer
   }
-    
+  
   private func startTimeWithDurtion (duration :Float, repeats :Bool, target :AnyObject, selector :Selector) {
     self.fireTarget = target
     self.fireSelector = selector
@@ -26,26 +27,38 @@ class SHTimer: NSObject {
     timer.startTimeWithDurtion(duration, repeats: repeats, fireBlock: fireBlock)
     return timer
   }
-
+  
   private func startTimeWithDurtion (duration :Float, repeats :Bool, fireBlock :(() -> Void)!) {
     self.fireBlock = fireBlock
     self.timer = NSTimer.scheduledTimerWithTimeInterval(Double(duration), target: self, selector: Selector("timerFired"), userInfo: nil, repeats: repeats)
   }
   
+  class func startTimeWithDurtion (duration :Float, repeats :Bool, function :() -> Void) -> SHTimer {
+    let timer = SHTimer()
+    timer.startTimeWithDurtion(duration, repeats: repeats, function: function)
+    return timer
+  }
+  
+  private func startTimeWithDurtion (duration :Float, repeats :Bool, function :(() -> Void)!) {
+    self.fireFunction = function
+    self.timer = NSTimer.scheduledTimerWithTimeInterval(Double(duration), target: self, selector: Selector("timerFired"), userInfo: nil, repeats: repeats)
+  }
+  
   func timerFired () {
-    performUpdateBlock()
     performUpdateSelector()
+    performUpdateFunction(self.fireFunction)
+    performUpdateFunction(self.fireBlock)
+  }
+  
+  func performUpdateFunction (function :(() -> Void)?) {
+    if let updateFunction = function {
+      updateFunction ()
+    }
   }
   
   func performUpdateSelector () {
     if self.fireSelector != nil && self.fireTarget != nil {
-      self.fireTarget!.sendAction(self.fireSelector!, to: self.fireTarget!, forEvent: nil)
-    }
-  }
-  
-  func performUpdateBlock () {
-    if self.fireBlock != nil {
-      self.fireBlock!()
+      UIControl().sendAction(self.fireSelector!, to: self.fireTarget!, forEvent: nil)
     }
   }
   
