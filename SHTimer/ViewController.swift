c74314a4a2920816e90d83e48c3a68def5ab0dc6
Tom Bates <tom.bates@onthebeach.co.uk>
2016-09-23 18:22:47 +0100

    Updated podspec

--- SHTimer/ViewController.swift
+++ SHTimer/ViewController.swift
@@ -1,25 +1,3 @@
-//
-//  ViewController.swift
-//  SHTimer
-//
-//  Created by Tom Bates on 04/01/2015.
-//  Copyright (c) 2015 SportHub. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
-}
-
+class ViewController: UIViewController {}
